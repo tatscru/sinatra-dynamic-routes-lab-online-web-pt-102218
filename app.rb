19a12a0b81a5e6d63c70e3f1d2a1bc6267da3cfd
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 21:45:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,36 +29,36 @@ class App < Sinatra::Base
   
   get '/:operation/:num1/:num2' do 
     @operation = params[:operation]
-    @num1 = params[:num1].to_i
-    @num2  = params[:num2].to_i
+    @number1 = params[:number1].to_i
+    @number2  = params[:number2].to_i
     
     case operation 
     when "add"
-      (num1 + num2).to_s
+      (number1 + number2).to_s
     when "subtract"
-      (num2 - num1).to_s
+      (number2 - number1).to_s
     when "multiply"
-      (num1 * num2).to_s
+      (number1 * number2).to_s
     when "divide"
-      (num1 / num2).to_s
+      (number1 / number2).to_s
     end 
   end 
   
-  get '/:operation/:number1/:number2' do
-		operation = params[:operation]
-		number1 = params[:number1].to_i
-		number2 = params[:number2].to_i
+#   get '/:operation/:number1/:number2' do
+# 		operation = params[:operation]
+# 		number1 = params[:number1].to_i
+# 		number2 = params[:number2].to_i
 
-		case operation
-		when 'add'
-			(number1 + number2).to_s
-		when 'subtract'
-			(number1 - number2).to_s
-		when 'multiply'
-			(number1 * number2).to_s
-		when 'divide'
-			(number1 / number2).to_s
-		end
-	end
+# 		case operation
+# 		when 'add'
+# 			(number1 + number2).to_s
+# 		when 'subtract'
+# 			(number1 - number2).to_s
+# 		when 'multiply'
+# 			(number1 * number2).to_s
+# 		when 'divide'
+# 			(number1 / number2).to_s
+# 		end
+# 	end
   
 end
