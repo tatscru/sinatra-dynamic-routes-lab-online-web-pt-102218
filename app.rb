fc1be3592552f1d086842375bcaca860b9112cc5
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 22:13:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,14 +15,22 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     @phrase = params[:phrase]
-    @number = params[:number]
+    @number = params[:number].to_i
     
     result = ""
 		@number.times do
 		  result += @phrase
     end  
     result 
-  end 
+    
+  #   @number = params[:number].to_i
+		# @phrase = params[:phrase]
+		# final_string = ""
+		# @number.times do
+		# 	final_string += @phrase
+		# end
+		# final_string
+  # end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     word1 = params[:word1]
