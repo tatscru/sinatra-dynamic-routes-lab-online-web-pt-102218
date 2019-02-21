5823e1f4ad228b98fa292e9d6558b5f862615de7
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 21:50:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -44,21 +44,6 @@ class App < Sinatra::Base
     end 
   end 
   
-#   get '/:operation/:number1/:number2' do
-# 		operation = params[:operation]
-# 		number1 = params[:number1].to_i
-# 		number2 = params[:number2].to_i
 
-# 		case operation
-# 		when 'add'
-# 			(number1 + number2).to_s
-# 		when 'subtract'
-# 			(number1 - number2).to_s
-# 		when 'multiply'
-# 			(number1 * number2).to_s
-# 		when 'divide'
-# 			(number1 / number2).to_s
-# 		end
-# 	end
   
 end
