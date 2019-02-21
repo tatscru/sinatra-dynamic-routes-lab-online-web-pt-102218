2d7736737295aa74c9943dd3ae5b026ae02e95ec
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 20:58:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,39 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end 
+  
+  get '/square/:number' do 
+    @number = params[:number].to_i
+    (@number * @number).to_s
+    # Multiply the number by itself= square 
+  end 
+  
+  get '/say/:number/:phrase' do 
+    @phrase = params[:phrase]
+  end 
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    
+    "#{word1} #{word2} #{word3} #{word4} #{word5}."
+  end 
+  
+  get '/:operation/:num1/:num2' do 
+    @operation = params[:operation]
+    @num1 = params[:num1]
+    @num2  = params[:num2]
+    (@num1 + @num2).to_i
+    (@num2 - @num1).to_i
+    (@num1 * @num2).to_i 
+    (@num1 % @num2).to_i
+  end 
+  
 end
