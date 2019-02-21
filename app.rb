b11d877938cf1e4250193e1acf7df085c462f22c
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-21 22:14:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
 		# 	final_string += @phrase
 		# end
 		# final_string
-  # end 
+  end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     word1 = params[:word1]
